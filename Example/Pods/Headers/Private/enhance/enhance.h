39feff4ad89547ed08cd4a2392082be802dbe47e
Adam Yanalunas <adamy@amcoonline.net>
2016-09-07 15:56:35 -0700

    Update Podfile for  1.x format, Travis to use Xcode 7.3

---
+++ Example/Pods/Headers/Private/enhance/enhance.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/enhance.h
--- Example/Pods/Target Support Files/Pods-enhance-enhance/Pods-enhance-enhance-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_enhance_enhance : NSObject
-@end
-@implementation PodsDummy_Pods_enhance_enhance
-@end
